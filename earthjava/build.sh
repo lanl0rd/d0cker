245101a2c8e7d9b2a1463fe6a6dae0bed8c412c3
lanl0rd <k1ng@glass.earth>
2021-02-15 00:41:47 -0500

    unzip and other stuffs

--- earthjava/build.sh
+++ earthjava/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-   --rm --compress -t lanl0rd/earthjava:latest -f ./Dockerfile . \
+   --rm --push --compress -t lanl0rd/earthjava:latest -f ./Dockerfile . \
     && docker save lanl0rd/earthjava:latest | gzip > earthjava.tar.gz
--- earthpy/build.sh
+++ earthpy/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-   --rm --compress -t lanl0rd/earthpy:latest -f ./Dockerfile . \
+   --rm --push --compress -t lanl0rd/earthpy:latest -f ./Dockerfile . \
     && docker save lanl0rd/earthpy:latest | gzip > earthpy.tar.gz
