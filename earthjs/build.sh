5231f4f4bf5e96cb8ab212ef1b182399eed48892
lanl0rd <k1ng@glass.earth>
2021-02-15 16:39:41 -0500

    hopefully this works

--- earthjs/build.sh
+++ earthjs/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-    --rm --push --compress --cache-from=node:latest -t lanl0rd/earthjs:latest -f ./Dockerfile . \
+    --rm --load --compress --cache-from=node:latest -t lanl0rd/earthjs:latest -f ./Dockerfile . \
     && docker save lanl0rd/earthjs:latest | gzip > earthjs.tar.gz
--- earthpy/build.sh
+++ earthpy/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-   --rm --push --compress -t lanl0rd/earthpy:latest -f ./Dockerfile . \
+   --rm --load --compress -t lanl0rd/earthpy:latest -f ./Dockerfile . \
     && docker save lanl0rd/earthpy:latest | gzip > earthpy.tar.gz
