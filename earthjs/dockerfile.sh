17246045b7bdabd6083db23a4b1fcc70a728dcf0
lanl0rd <k1ng@glass.earth>
2021-02-16 18:22:18 -0500

    .

--- earthjs/dockerfile.sh
+++ earthjs/dockerfile.sh
@@ -5,6 +5,7 @@ command="add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/
 if [[ "$TARGETPLATFORM" == *"'arm'"* ]] ; then command='add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'; fi
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
+echo "this is command: $command" && \
     apt-get update && \
     apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
     apt-get update && \
