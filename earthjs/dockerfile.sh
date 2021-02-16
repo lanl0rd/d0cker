f015b7fafbe250cde8f97d91ac15763515680fda
lanl0rd <k1ng@glass.earth>
2021-02-16 18:17:41 -0500

    .

--- earthjs/dockerfile.sh
+++ earthjs/dockerfile.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+
+# add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"
+# if [ "$TARGETPLATFORM" == *"'amd'"* ] ; then add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; else add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; fi
+
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
     apt-get update && \
-    apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common&& \
+    apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
     apt-get update && \
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
-    # if [ "$TARGETPLATFORM" == *"'amd'"* ] ; then add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; else add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; fi && \
     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" && \
     apt-get update && \
-    apt-get install -y docker-ce docker-ce-cli containerd.io  && \
+    # apt-get install -y docker-ce docker-ce-cli containerd.io  && \
     pip3 install docker-compose && \
     pip3 install crossplane && \
-    npm i -g yarn && \
+    npm i -g yarn || true && \
     npm i -g typescript && \
     npm i -g rimraf && \
     npm i -g @nestjs/cli && \
