e745700cb230904a0f47ed672e1133c92b854901
lanl0rd <k1ng@glass.earth>
2021-02-16 17:56:08 -0500

    do

--- earthjs/dockerfile.sh
+++ earthjs/dockerfile.sh
@@ -5,7 +5,7 @@ echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
     apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common&& \
     apt-get update && \
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
-    if [[ "$TARGETPLATFORM" == *"$amd"* ]] ; then sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; else sudo add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; fi && \
+    if [[ "$TARGETPLATFORM" == *"'amd'"* ]] ; then echo "amd..." && add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; else echo "arm..."  && add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; fi && \
     apt-get update && \
     apt-get install -y docker-ce docker-ce-cli containerd.io  && \
     pip3 install docker-compose && \
