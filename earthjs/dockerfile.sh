85b98d45500d8ded3da6cdcc00696e39d3056803
lanl0rd <k1ng@glass.earth>
2021-02-16 17:48:22 -0500

    do

--- earthjs/dockerfile.sh
+++ earthjs/dockerfile.sh
@@ -2,8 +2,7 @@
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
     apt-get update && \
-    apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils && \
-    apt-get install -y apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
+    apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common&& \
     apt-get update && \
     curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
     if [[ "$TARGETPLATFORM" == *"$amd"* ]] ; then add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; else add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"; fi && \
