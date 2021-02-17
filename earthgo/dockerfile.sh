8c313a91a6f7a3d48c261a1c5fd5e965dd51aa8e
lanl0rd <k1ng@glass.earth>
2021-02-16 19:15:50 -0500

    vegan

---
+++ earthgo/dockerfile.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+
+command="add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'"
+if [[ "$TARGETPLATFORM" == *"'arm'"* ]] ; then command='add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'; fi
+    
+apt-get update && \
+    apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
+    apt-get update && \
+    curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add - && \
+    eval "$command" && \
+    apt-get update && \
+    apt-get install -y docker-ce docker-ce-cli containerd.io && \
+    go get -u github.com/gin-gonic/gin && \
+    go get github.com/beego/beego/v2@v2.0.0 && \
+    go get github.com/revel/revel && \
+    go get -u github.com/valyala/fasthttp && \
+    go get goji.io && \
+    go get github.com/go-martini/martini && \
+    go get github.com/gorilla/mux && \
+    go get github.com/labstack/echo/v4 && \
+    go get github.com/urfave/negroni && \
+    go get github.com/go-kit/kit && go get github.com/kujtimiihoxha/kit && \
+    wget https://github.com/gobuffalo/buffalo/releases/download/v0.16.21/buffalo_0.16.21_Linux_x86_64.tar.gz && tar -xvzf buffalo_0.16.21_Linux_x86_64.tar.gz && mv buffalo /usr/local/bin/buffalo
