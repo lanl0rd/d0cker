f9174ac185cedf2146f2b42b2db4698fa0014e2b
lanl0rd <k1ng@glass.earth>
2021-02-17 09:09:52 -0500

    push images by default now as there is oci error trying to store multiple arches locally, perhaps i need docker repositories now

--- earthruby/build.sh
+++ earthruby/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-   --rm --load --compress -t lanl0rd/earthruby:latest -f ./Dockerfile . \
+   --rm --push --compress -t lanl0rd/earthruby:latest -f ./Dockerfile . \
     && docker save lanl0rd/earthruby:latest | gzip > earthruby.tar.gz
