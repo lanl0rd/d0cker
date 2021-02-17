3a139e162028f9028e5d9f0e208cf448961a40fa
lanl0rd <k1ng@glass.earth>
2021-02-16 20:08:22 -0500

    .

--- earthpy/dockerfile.sh
+++ earthpy/dockerfile.sh
@@ -3,12 +3,13 @@ export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 command="add-apt-repository" '"deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'
 if [[ "$TARGETPLATFORM" == *"'arm'"* ]] ; then command="add-apt-repository" '"deb [arch=arm64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'; fi;
+echo $command
 
 apt-get update && \
     apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
     apt-get update && \
-    eval "curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -" && \
-    eval "$command" && \
+    # eval "curl -fsSL https://download.docker.com/linux/debian/gpg | apt-key add -" && \
+    # eval "$command" && \
     apt-get update && \
     apt-get install -y docker-ce docker-ce-cli containerd.io && \
     pip3 install docker-compose && \
