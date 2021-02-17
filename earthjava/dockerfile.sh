67ac93b7f224d33f3bafce01335d3642988d9f06
lanl0rd <k1ng@glass.earth>
2021-02-16 20:32:48 -0500

    .

--- earthjava/dockerfile.sh
+++ earthjava/dockerfile.sh
@@ -2,8 +2,9 @@
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 command="add-apt-repository 'deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable'"
-if [[ "$TARGETPLATFORM" == *"'arm'"* ]] ; then command='add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'; fi
+if [[ "$TARGETPLATFORM" == *"'arm'"* ]] ; then command='add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable"'; fi;
 
+echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
 apt-get update && \
     apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils maven dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
     apt-get update && \
