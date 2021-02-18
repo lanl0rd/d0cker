9a212603b704abae23009fb2ff873b6880c263c7
lanl0rd <k1ng@glass.earth>
2021-02-17 21:36:37 -0500

    dpkg architecture

--- ruby/dockerfile.sh
+++ ruby/dockerfile.sh
@@ -6,7 +6,7 @@ export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 command="add-apt-repository \"deb [arch=amd64] https://download.docker.com/linux/debian buster stable\""
 if [[ "$TARGETPLATFORM" == *"arm"* ]] ; then command="add-apt-repository \"deb [arch=arm64] https://download.docker.com/linux/debian buster stable\"" ; fi;
 
-echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
+echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM " && "dpkg --print-architecture" && \
 apt-get update && \
     apt-get install -y curl git wget rsync openssl mergerfs sshfs zip unzip python3-pip dnsutils dialog apt-utils apt-transport-https ca-certificates  gnupg-agent software-properties-common && \
     apt-get update && \
