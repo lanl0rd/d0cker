206a5e349e58b14acaaa1624ed9f2bdcd4cbee4f
lanl0rd <k1ng@glass.earth>
2021-03-02 19:06:33 -0500

    shows

--- certbot/dockerfile.sh
+++ certbot/dockerfile.sh
@@ -2,6 +2,8 @@
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
+    apt-get instayll -y software-properties-common && \
+    apt-get update && \
     add-apt-repository ppa:certbot/certbot && \
     apt-get update && \
     apt-get install -y certbot
