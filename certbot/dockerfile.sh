d97564479bb68fc3ae1b8e0a3ddb868a6b86bca0
lanl0rd <k1ng@glass.earth>
2021-03-02 22:17:54 -0500

    chiraq

--- certbot/dockerfile.sh
+++ certbot/dockerfile.sh
@@ -2,9 +2,5 @@
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
-    apt update && apt install -y snapd && \
-    systemctl restart snapd.service && \
-    apt update && \
-    snap install --classic certbot && \
-    ln -s /snap/bin/certbot /usr/bin/certbot && \
-    snap install certbot-dns-cloudflare
+    apt-get update -q && \
+    apt-get install certbot -qy
