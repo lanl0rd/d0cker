a22d94b077975576890db12a2cd5a2578f931e44
lanl0rd <k1ng@glass.earth>
2021-03-02 22:09:15 -0500

    news

--- certbot/dockerfile.sh
+++ certbot/dockerfile.sh
@@ -2,6 +2,8 @@
 export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
+    apt update && apt install -y snapd && \
+    systemctl restart snapd.service && \
     apt update && \
     snap install --classic certbot && \
     ln -s /snap/bin/certbot /usr/bin/certbot && \
