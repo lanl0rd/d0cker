78c2ccbf5fb734f35e058b9015d3bf411652e174
k1ng <k1ng@glass.earth>
2021-03-31 20:08:20 +0000

    mary

--- frp/dockerfile.sh
+++ frp/dockerfile.sh
@@ -1,14 +1,19 @@
+export APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1
+
 frp_download="https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_amd64.tar.gz"
 frp="frp_0.36.2_linux_amd64"
 
 if [[ "$TARGETPLATFORM" == *"arm"* ]] ; then frp_download="https://github.com/fatedier/frp/releases/download/v0.36.2/frp_0.36.2_linux_arm64.tar.gz"; frp="frp_0.36.2_linux_arm64"; fi ;
 
-echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM"
-
-wget -P /tmp/docker $frp_download
 cd /tmp/docker
+pwd
+ls -la
+apt update
+apt-get install -y wget
+apt update
+echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM"
+wget $frp_download
 tar -xf ${frp}.tar.gz
 pwd
 ls -la
 mv ${frp} /etc/frp
-rm -rf /tmp/docker
