fe4227a0177e68a06def88f97f795d144f5996fe
simba <k1ng@glass.earth>
2021-03-31 15:01:14 -0400

    okay

--- frp/dockerfile.sh
+++ frp/dockerfile.sh
@@ -2,11 +2,10 @@ frp="frp_amd64"
 if [[ "$TARGETPLATFORM" == *"arm"* ]] ; then frp="frp_arm64"; fi ;
 
 echo "I am running on $BUILDPLATFORM, building for $TARGETPLATFORM" && \
-echo "okayyyy"
 
 cd /tmp/docker && \
 pwd && \
 ls -la && \
 tar -xf ${frp}.tar.gz && \
-mv ${frp} /frp && \
+mv ${frp} /etc/frp && \
 rm -rf /tmp/docker
