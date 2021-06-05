ae0fc54a5acf9059f9ab5467fd75c1f7b0eed576
lanl0rd <k1ng@glass.earth>
2021-06-04 23:48:20 -0400

    fresH

--- face/build.sh
+++ face/build.sh
@@ -1,3 +1,3 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-    --rm --push --compress -t lanl0rd/js:latest -f ./Dockerfile . \
-    && docker save lanl0rd/js:latest | gzip > js.tar.gz
+    --rm --push --compress -t lanl0rd/face:fresh -f ./Dockerfile . \
+    && docker save lanl0rd/face:fresh | gzip > face-fresh.tar.gz
