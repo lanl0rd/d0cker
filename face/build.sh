5ca80e25daf207dee379062d712d56b2cf155f4f
lanl0rd <k1ng@glass.earth>
2021-06-05 10:39:38 -0400

    .

--- face/build.sh
+++ face/build.sh
@@ -1,3 +1,1 @@
-docker buildx build --platform linux/arm64,linux/amd64 \
-    --rm --push --compress -t lanl0rd/face:fresh -f ./Dockerfile . \
-    && docker save lanl0rd/face:fresh | gzip > face-fresh.tar.gz
+docker buildx build --platform linux/arm64,linux/amd64 --rm --push --compress -t lanl0rd/face:fresh -f ./Dockerfile .
