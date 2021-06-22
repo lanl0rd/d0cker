0703e0d7c0aa1766a0b3d01d5b2bb0239a182d30
lanl0rd <k1ng@glass.earth>
2021-06-22 12:14:29 -0400

    sh

--- js/build.sh
+++ js/build.sh
@@ -1,3 +1,2 @@
 docker buildx build --platform linux/arm64,linux/amd64 \
-    --rm --push --compress -t lanl0rd/js:latest -f ./Dockerfile . \
-    && docker save lanl0rd/js:latest | gzip > js.tar.gz
+    --rm --push --compress -t lanl0rd/js:nvm -f ./Dockerfile .
