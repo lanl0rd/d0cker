d0e3f39241594be83c013017cfd82e0576c6aa72
lanl0rd <k1ng@glass.earth>
2021-06-28 21:16:36 -0400

    latest

--- acme/build.sh
+++ acme/build.sh
@@ -1,1 +1,1 @@
-docker buildx build --platform linux/arm64,linux/amd64 --rm --push --compress -t lanl0rd/acme:test -f ./Dockerfile .
+docker buildx build --platform linux/arm64,linux/amd64 --rm --push --compress -t lanl0rd/acme:latest -f ./Dockerfile .
